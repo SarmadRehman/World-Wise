cbc0a4476cd81cd4c94cb7207a09e86bf6131c99
Sarmad Rehman <sarmadr02@gmail.com>
2024-07-16 16:16:39 +0500

    Programmatic navigation through useNavigate between Map and Form

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import styles from "./Button.module.css";
+const Button = ({ children, onClick, type }) => {
+  return (
+    <div onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Button;
